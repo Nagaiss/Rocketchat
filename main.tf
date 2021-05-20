7c6e13d45e25fdcb4d1f1b3a6ba16df0a01c7afc
nagaiss <70169215+Nagaiss@users.noreply.github.com>
2021-05-20 07:40:33 -0700

    terraform push final correction

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region  = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-west-2"
 }
 
 resource "aws_key_pair" "deployer" {
-  key_name = "${var.key_name}"
-  public_key = "${var.public_key}"
+  key_name   = var.key_name
+  public_key = var.public_key
 }
 
 data "aws_ami" "ubuntu" {
@@ -22,24 +22,18 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   count         = var.instance_count
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.deployer.key_name}"
-  
-  provisioner "remote-exec" {
-    connection {
-	  type         = "ssh"
-	  user         = "ubuntu"
-	  private_key  = "${file(var.private_key)}"
-	  agent        = true
-	}
-	
-    inline = [
-	  "sudo snap install rocketchat-server",
-	  "sudo rocketchat-server.initcaddy",
-	]
+  key_name      = aws_key_pair.deployer.key_name
 
-  tags = {
-    Name = "RocketChatServer"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo snap install rocketchat-server",
+      "sudo rocketchat-server.initcaddy",
+    ]
   }
-}
+  
+    tags = {
+      Name = "RocketChatServer"
+    }
+  }
