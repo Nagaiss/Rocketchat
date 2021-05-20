ea10ab8fa929d7a1d0c5857152c15bf02aaab23c
nagaiss <70169215+Nagaiss@users.noreply.github.com>
2021-05-20 09:43:32 -0700

    terraform push final revert revert

--- main.tf
+++ main.tf
@@ -7,13 +7,26 @@ terraform {
   }
 }
 
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
 resource "aws_instance" "app_server" {
   count         = var.instance_count
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  
+  provisioner "remote-exec" {
+
+    inline = [
+	  "sudo snap install rocketchat-server",
+	]
 
 
-  tags = {
+
+ }
+   tags = {
     Name = "RocketChatServer"
   }
 }
