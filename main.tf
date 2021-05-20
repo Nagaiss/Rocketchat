dd8f9eed8e0bcb52041713646576f74e8a1b261f
nagaiss <70169215+Nagaiss@users.noreply.github.com>
2021-05-20 07:15:34 -0700

    terraform push final

--- main.tf
+++ main.tf
@@ -1,21 +1,43 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region  = "us-west-2"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+resource "aws_key_pair" "deployer" {
+  key_name = "${var.key_name}"
+  public_key = "${var.public_key}"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
 }
 
 resource "aws_instance" "app_server" {
   count         = var.instance_count
-  ami           = "ami-830c94e3"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.deployer.key_name}"
+  
+  provisioner "remote-exec" {
+    connection {
+	  type         = "ssh"
+	  user         = "ubuntu"
+	  private_key  = "${file(var.private_key)}"
+	  agent        = true
+	}
+	
+    inline = [
+	  "sudo snap install rocketchat-server",
+	  "sudo rocketchat-server.initcaddy",
+	]
 
   tags = {
     Name = "RocketChatServer"
